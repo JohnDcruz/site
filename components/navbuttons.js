e796c16002ef5a943bf7416b3b190019da04b776
John Dcruz <johndcruz@johndcruz.com>
2022-08-15 01:13:13 -0400

    updated links

--- components/navbuttons.js
+++ components/navbuttons.js
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode, faInfo, faFilm } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 export default function NavButtons({ showText }) {
   let text = { about: '', media: '', dev: '' };
@@ -8,15 +9,21 @@ export default function NavButtons({ showText }) {
   }
   return (
     <span>
-      <a href='/about' className='text-xl mt-4 py-2 px-4 mx-4 rounded-full border-0 font-semibold bg-slate-100 text-slate-800 hover:bg-slate-100'>
-        <FontAwesomeIcon icon={faInfo} />{text.about}
-      </a>
-      <a href='/portfolio/media' className='text-xl mt-4 py-2 px-4 mx-4 rounded-full border-0 font-semibold bg-slate-100 text-slate-800 hover:bg-slate-100'>
-        <FontAwesomeIcon icon={faFilm} />{text.media}
-      </a>
-      <a href='/portfolio/dev' className='text-xl mt-4 py-2 px-4 mx-4 rounded-full border-0 font-semibold bg-slate-100 text-slate-800 hover:bg-slate-100'>
-        <FontAwesomeIcon icon={faCode} />{text.dev}
-      </a>
+      <Link href='/about'>
+        <button className='text-xl mt-4 py-2 px-4 mx-4 rounded-full border-0 font-semibold bg-slate-100 text-slate-800 hover:bg-slate-100'>
+          <FontAwesomeIcon icon={faInfo} />{text.about}
+        </button>
+      </Link>
+      <Link href='/portfolio/media'>
+        <button className='text-xl mt-4 py-2 px-4 mx-4 rounded-full border-0 font-semibold bg-slate-100 text-slate-800 hover:bg-slate-100'>
+          <FontAwesomeIcon icon={faFilm} />{text.media}
+        </button>
+      </Link>
+      <Link href='/portfolio/dev'>
+        <button className='text-xl mt-4 py-2 px-4 mx-4 rounded-full border-0 font-semibold bg-slate-100 text-slate-800 hover:bg-slate-100'>
+          <FontAwesomeIcon icon={faCode} />{text.dev}
+        </button>
+      </Link>
     </span>
   )
 }
