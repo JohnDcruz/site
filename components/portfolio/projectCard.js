cb7334ac37303e9cd041c0d417c498bac692ab13
John Dcruz <johndcruz@johndcruz.com>
2022-08-18 22:03:37 -0400

    fixed bugs

--- components/portfolio/projectCard.js
+++ components/portfolio/projectCard.js
@@ -2,7 +2,7 @@ export default function ProjectCard({ project }) {
   return (
     <div className="p-12 text-center relative overflow-hidden bg-no-repeat bg-cover rounded-lg"
     style={{
-      backgroundImage: `url(${'https:' + project.thumbnail.fields.file.url})`,
+      backgroundImage: `url(${project.thumbnail ? 'https:' + project.thumbnail.fields.file.url : ''})`,
       height: `400px`,
     }}>
     <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed">
