c21fd803c4417d01d7bdb885334505b151d14111
John Dcruz <johndcruz@johndcruz.com>
2022-11-17 17:46:28 -0500

    updated list efficiency

--- components/helpers/listHelper.js
+++ components/helpers/listHelper.js
@@ -1,8 +1,3 @@
 export default function getListWithCommas(list) {
-  let listString = '';
-  for (let i = 0; i < list.length - 1; i++) {
-    listString += list[i] + ', ';
-  }
-  listString += list[list.length - 1];
-  return listString;
+  return list.join(', ');
 }
