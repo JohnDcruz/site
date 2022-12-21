013665db6d3cbc708837a7fd8f3511e4dedbc097
JohnDcruz <johndcruz@johndcruz.com>
2022-12-21 16:58:50 -0500

    updated coming soon

--- pages/comingsoon.js
+++ pages/comingsoon.js
@@ -7,7 +7,7 @@ export default function Home() {
 
       <main className='mx-auto h-screen flex flex-col bg-slate-800 items-center text-center text-white justify-center'>
         <h1 className='font-serif tracking-wide text-4xl mb-10 text-center'>
-          I&apos;m still working on this page, but it&apos;ll be here soon. In the meantime, let&apos;s get you home...
+          Please excuse our appearance while we renovate.
         </h1>
         <meta httpEquiv="refresh" content="2;URL='/'" />    
       </main>
