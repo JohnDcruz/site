774600a663e43f93f7074130a387b94168ccd318
JohnDcruz <johndcruz@johndcruz.com>
2023-12-07 19:11:30 -0500

    added speakerschedule page

---
+++ pages/speakerschedule.js
@@ -0,0 +1,12 @@
+function SpeakerSchedule() {
+  return (
+    <div className='p-4'>
+      <h1>SpeakerSchedule App Information</h1>
+      <p>Email support@johndcruz.com for help.</p>
+      <h2>Privacy Policy</h2>
+      <p>The app does not collect any information beyond the information users provide. Data is stored only for the purposes of app functionality, and is not used beyond the app.</p>
+    </div>
+  )
+}
+
+export default SpeakerSchedule;
