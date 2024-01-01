0042e1df79877894b08c78b0373c7c694fb398be
JohnDcruz <johndcruz@johndcruz.com>
2024-01-01 18:36:24 +0530

    updated 404

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,19 @@
 import Header from "../components/header"
+import Router from "next/router"
 
-export default function Home() {
+export default function PageNotFound() {
+  setTimeout(() => {
+    Router.push("/")
+  }, 2000)
+
   return (
     <div>
       <Header title={'404'} />
 
       <main className='mx-auto h-screen flex flex-col bg-slate-800 items-center text-center text-white justify-center'>
-        <h1 className='font-serif tracking-wide text-4xl mb-10 text-center'>
+        <h1 className='text-4xl m-5 text-center'>
           We couldn&apos;t find that page! Let&apos;s get you home...
         </h1>
-        <meta httpEquiv="refresh" content="2;URL='/'" />    
       </main>
     </div>
   )
