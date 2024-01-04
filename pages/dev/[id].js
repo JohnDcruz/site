81a45632656cf85c349cc3299f95666fb98a58a6
JohnDcruz <johndcruz@johndcruz.com>
2024-01-04 17:56:15 -0500

    updated dev page

--- pages/dev/[id].js
+++ pages/dev/[id].js
@@ -12,7 +12,7 @@ export default function DevPage() {
       <Header title={project ? project.title : 'CS Project'} />
       <main className='mx-auto flex flex-col bg-slate-800 items-center text-white justify-center h-full'>
         <div className="m-5">
-          <p className='font-bold text-4xl text-center'>{project.title} coming soon...</p>
+          <p className='font-bold text-4xl text-center'>{project ? project.title : null} coming soon...</p>
         </div>
       </main>
     </div>
