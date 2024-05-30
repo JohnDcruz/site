f17844e397147b1ce3ae434cbf5650b0b9839001
JohnDcruz <johndcruz@johndcruz.com>
2024-05-30 15:19:03 -0400

    updated profile

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Header from "../components/header"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot, faEnvelope, faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faLocationDot, faEnvelope, faClapperboard } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin, faImdb } from "@fortawesome/free-brands-svg-icons";
 import Link from "next/link";
 import { Tooltip } from "flowbite-react";
 
@@ -10,44 +10,38 @@ export default function Home() {
     <div>
       <Header title={'Home'}/>
 
-      <main className='mx-auto h-screen flex flex-col bg-slate-800 items-center text-center text-white justify-center relative'>
-        <p className='font-serif tracking-wide text-7xl mb-6 text-center hover:text-red-400'>
+      <main className='mx-auto h-screen flex flex-col bg-slate-800 items-center text-center text-white justify-center relative space-y-5'>
+        <p className='font-serif tracking-wide text-7xl text-center mb-2 hover:text-red-400'>
           <Link href='about'>
             john d&apos;cruz
           </Link>
         </p>
-        <p className='text-2xl font-serif tracking-wide mb-5'>
-          <span className='hover:text-blue-400'><Link href='/film'>filmmaker.</Link>&nbsp;</span>
-          <span className='hover:text-green-400'><Link href='/terminal'>developer.</Link>&nbsp;</span>
-          <span className='hover:text-yellow-400'><Link href='/about'>student.</Link></span>
+        <p className='text-3xl font-serif tracking-wide'>
+          <span className='hover:text-blue-400'><Link href='/film'>filmmaker</Link></span>
+          <span className='mx-2'>|</span>
+          <span className='hover:text-green-400'><Link href='/terminal'>developer</Link></span>
         </p>
-        <div className="flex gap-2 mb-5">
-          <Tooltip content='New York Metropolitan Area' placement="bottom">
-            <button id='locationButton' className='text-xl mt-4 py-2 px-4 mx-4 rounded-full border-0 font-semibold bg-slate-100 text-slate-800 hover:bg-slate-300'>
-              <FontAwesomeIcon icon={faLocationDot} />
+        <div className="flex gap-4 text-2xl">
+          <Link href='mailto:johndcruz@johndcruz.com' rel="noopener noreferrer" target="_blank">
+            <button id='emailButton' className='mt-4 py-2 px-4 mx-4 rounded-full border-0 font-semibold bg-slate-100 text-slate-800 hover:bg-slate-300'>
+              <FontAwesomeIcon icon={faEnvelope} />
             </button>
-          </Tooltip>
-          <Tooltip content='johndcruz@johndcruz.com' placement="bottom">
-            <Link href='mailto:johndcruz@johndcruz.com'>
-              <button id='emailButton' className='text-xl mt-4 py-2 px-4 mx-4 rounded-full border-0 font-semibold bg-slate-100 text-slate-800 hover:bg-slate-300'>
-                <FontAwesomeIcon icon={faEnvelope} />
+          </Link>
+          <Link href='https://www.imdb.com/name/nm14679028' rel="noopener noreferrer" target="_blank">
+            <button id='imdbButton' className='mt-4 py-2 px-4 mx-4 rounded-full border-0 font-semibold bg-slate-100 text-slate-800 hover:bg-slate-300'>
+              <FontAwesomeIcon icon={faClapperboard} />
             </button>
-            </Link>
-          </Tooltip>
-          <Tooltip content='Github' placement="bottom">
-            <Link href='https://github.com/johndcruz'>
-              <button id='githubButton' className='text-xl mt-4 py-2 px-4 mx-4 rounded-full border-0 font-semibold bg-slate-100 text-slate-800 hover:bg-slate-300'>
-                <FontAwesomeIcon icon={faGithub} />
-              </button>
-            </Link>
-          </Tooltip>
-          <Tooltip content='LinkedIn' placement="bottom">
-            <Link href='https://www.linkedin.com/in/john-d-cruz-1225011b0/'>
-              <button id='linkedinButton' className='text-xl mt-4 py-2 px-4 mx-4 rounded-full border-0 font-semibold bg-slate-100 text-slate-800 hover:bg-slate-300'>
-                <FontAwesomeIcon icon={faLinkedin} />
-              </button>
-            </Link>
-          </Tooltip>
+          </Link>
+          <Link href='https://github.com/johndcruz' rel="noopener noreferrer" target="_blank">
+            <button id='githubButton' className='mt-4 py-2 px-4 mx-4 rounded-full border-0 font-semibold bg-slate-100 text-slate-800 hover:bg-slate-300'>
+              <FontAwesomeIcon icon={faGithub} />
+            </button>
+          </Link>
+          <Link href='https://www.linkedin.com/in/john-d-cruz-1225011b0/' rel="noopener noreferrer" target="_blank">
+            <button id='linkedinButton' className='mt-4 py-2 px-4 mx-4 rounded-full border-0 font-semibold bg-slate-100 text-slate-800 hover:bg-slate-300'>
+              <FontAwesomeIcon icon={faLinkedin} />
+            </button>
+          </Link>
         </div>
       </main>
     </div>
