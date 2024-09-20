d0d240a192ddb33e2312bb12b0ea08ef98c8b7ab
JohnDcruz <johndcruz@johndcruz.com>
2024-09-20 15:08:44 -0700

    added new film projects

--- pages/film/index.js
+++ pages/film/index.js
@@ -5,7 +5,8 @@ import Image from "next/image";
 import { GoogleAnalytics } from '@next/third-parties/google'
 
 function Film() {
-  const projects = data.film_projects.filter((project) => project.own_page === true);
+  const stormy = data.film_projects.filter((project) => project.id === 'stormy')[0];
+  const projects = data.film_projects.filter((project) => project.own_page === true).sort((a, b) => (a.year < b.year) ? 1 : -1).filter((project) => project.id !== 'stormy');
   const router = useRouter();
 
   return (
@@ -13,8 +14,11 @@ function Film() {
       <Header title={"Film Work"} />
       <GoogleAnalytics gaId='G-GHQ9WRFG4H' />
 
-      <main className='mx-auto p-10 flex flex-col items-center text-white justify-center h-full'>
+      <main className='mx-auto p-10 flex flex-col items-center text-white justify-center h-full w-full'>
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4 m-5 justify-center items-center">
+          <div className="cursor-pointer grayscale-0 md:grayscale md:hover:grayscale-0 col-span-3" key={stormy.id} onClick={() => router.push('/film/' + stormy.id)}>
+            <Image src={'/film_images/' + stormy.id + '/thumb.jpg'} alt={stormy.title} width={1920} height={500} className="rounded-lg" />
+          </div>
           {projects.map((film) => (
             <div className="cursor-pointer grayscale-0 md:grayscale md:hover:grayscale-0" key={film.id} onClick={() => router.push('/film/' + film.id)}>
               <Image src={'/film_images/' + film.id + '/thumb.jpg'} alt={film.title} height={1000} width={1000} className="rounded-lg" />
